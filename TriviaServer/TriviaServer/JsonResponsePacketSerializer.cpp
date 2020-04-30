f66747cbe2d15e206ffdaf7942ef4abc921eff7d
Omri Zaiman <omri162004@gmail.com>
2020-04-30 12:50:50 +0300

    added jsonSerializer functions' initial definitions

--- TriviaServer/TriviaServer/JsonResponsePacketSerializer.cpp
+++ TriviaServer/TriviaServer/JsonResponsePacketSerializer.cpp
@@ -1,2 +1,19 @@
 #include "JsonResponsePacketSerializer.h"
 
+
+vector<unsigned char> JsonResponsePacketSerializer::serializeResponse(ErrorResponse e)
+{
+	return vector<unsigned char>();
+}
+
+
+vector<unsigned char> JsonResponsePacketSerializer::serializeResponse(LoginResponse e)
+{
+	return vector<unsigned char>();
+}
+
+
+vector<unsigned char> JsonResponsePacketSerializer::serializeResponse(SignupResponse e)
+{
+	return vector<unsigned char>();
+}
