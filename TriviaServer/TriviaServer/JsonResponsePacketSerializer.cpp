f8c4fdc117cbc7f22af64b7fe2f6c619ecc8b89f
Omri Zaiman <omri162004@gmail.com>
2020-05-03 12:15:20 +0300

    added castIDTOBin function

--- TriviaServer/TriviaServer/JsonResponsePacketSerializer.cpp
+++ TriviaServer/TriviaServer/JsonResponsePacketSerializer.cpp
@@ -5,7 +5,7 @@ The function will serialize a response object to a binary buffer that will be se
 input: a response object
 output: buffer that will be sent to the user
 */
-vector<unsigned char> JsonResponsePacketSerializer::serializeResponse(Message* response)
+vector<unsigned char> JsonResponsePacketSerializer::serializeResponse(Response* response)
 {
 	vector<unsigned char> message; // the whole message
 	vector<unsigned char> content; // part 3 of message
@@ -34,6 +34,26 @@ vector<unsigned char> JsonResponsePacketSerializer::castSizeToBin(int size)
 {
 	const int NUMBER_OF_BITS = 32; // 8 bits * 4
 	string sizeInBinary = bitset<NUMBER_OF_BITS>(size).to_string();
+	vector<unsigned char> result;
+
+	for (string::iterator it = sizeInBinary.begin(); it != sizeInBinary.end(); it++)
+	{
+		result.push_back((unsigned char)*it);
+	}
+
+	return result;
+}
+
+/*
+The function will cast an integer into vector of bytes with 4 elements.
+Example= 5 --> [00000000 00000000 00000000 00000101]
+input: a size to convert
+output: a converted size presented as a binary sequence with 4 bytes
+*/
+vector<unsigned char> JsonResponsePacketSerializer::castMsgCodeToBin(int msgCode)
+{
+	const int NUMBER_OF_BITS = 8; // 8 bits * 1 byte
+	string sizeInBinary = bitset<NUMBER_OF_BITS>(msgCode).to_string();
 	vector<unsigned char> result;
 
 	for (string::iterator it = sizeInBinary.begin(); it != sizeInBinary.end(); it++)
--- TriviaServer/TriviaServer/JsonResponsePacketSerializer.h
+++ TriviaServer/TriviaServer/JsonResponsePacketSerializer.h
@@ -3,7 +3,7 @@
 #include <vector>
 #include <string>
 #include <bitset>
-#include "Message.h"
+#include "Response.h"
 #include "json.hpp"
 
 using std::string;
@@ -15,9 +15,10 @@ using std::bitset;
 class JsonResponsePacketSerializer
 {
 public:
-	static vector<unsigned char> serializeResponse(Message* response);
+	static vector<unsigned char> serializeResponse(Response* response);
 
 private:
 	static vector<unsigned char> castSizeToBin(int size);
+	static vector<unsigned char> castMsgCodeToBin(int msgCode);
 
 };
