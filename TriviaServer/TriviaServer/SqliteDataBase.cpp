12cc501d405936de9e2f3ed9c9a9a811c924ca3c
Omri Zaiman <omri162004@gmail.com>
2020-05-06 19:37:41 +0300

    added OurDB.db file- contains the Users table

---
+++ TriviaServer/TriviaServer/SqliteDataBase.cpp
@@ -0,0 +1,29 @@
+#include "SqliteDataBase.h"
+
+/*
+The function will check if a user exists in the database by his username
+input: the user's username
+output: true or false whether the user exists or doesn't exist
+*/
+bool SqliteDataBase::doesUserExist(string username)
+{
+	return false;
+}
+
+/*
+The function will check if a user's password matches to a given password
+input: a password to check
+output: true or false whether the password does/n't match
+*/
+bool SqliteDataBase::doesPasswordMatch(string password)
+{
+	return false;
+}
+/*
+The funtion will add a new user to the users database
+input: user's details
+output: none
+*/
+void SqliteDataBase::addNewUser(string username, string password, string email)
+{
+}
