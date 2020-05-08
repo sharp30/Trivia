cc112b0993c3398daea11fc063b0953d37828f32
Omri Zaiman <omri162004@gmail.com>
2020-05-08 18:17:52 +0300

    added LoginManager class in .h file

--- TriviaServer/TriviaServer/LoginManager.h
+++ TriviaServer/TriviaServer/LoginManager.h
@@ -1,1 +1,14 @@
 #pragma once
+#include "IDatabase.h"
+
+class LoginManager
+{
+public:
+	void signup(string username, string password, string email) throw();
+	void login(string username, string password) throw();
+	void logout(string username) throw();
+
+private:
+	IDatabase* m_database;
+	//TODO: add loggedUsers vector here.
+};
