7f80d568a7123e73719ab9f60bd168a4a40e6123
Ofir Shapira <ofirs2003@gmail.com>
2020-06-15 16:50:08 +0300

    finish Design

--- TriviaWPFClient/TriviaClient/MainWindow.xaml.cs
+++ TriviaWPFClient/TriviaClient/MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -26,17 +27,35 @@ namespace TriviaClient
         public MainWindow()
         {
             InitializeComponent();
+            this.Hide();
+            bool isConnected = true;// CreateConnection();
+            if(isConnected)
+            {
+                MessageBoxResult  res = MessageBox.Show("Sorry man can't connect to server", "ERROR MESSAGE", MessageBoxButton.OKCancel, MessageBoxImage.Exclamation);
+                btn_quit.RaiseEvent(new RoutedEventArgs(ButtonBase.ClickEvent));
 
+            }
+            this.Show();
             
         }
-
-        private void SignInClick(object sender, RoutedEventArgs e)
+        
+        private void Btn_signin(object sender, RoutedEventArgs e)
         {
             this.username = TBUsername.Text;
-            this.password = TBPassword.Text;
+            this.password = TBPassword.Password;
 
             TBUsername.Text = "yay";
-            TBPassword.Text = "yay";
+            TBPassword.Password = "yay";
+            MenuWindow wind = new MenuWindow(username);
+            wind.Show();
+            this.Hide();
+            this.Close();
+
+
+        }
+        private void Btn_quit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
         }
 
         private void TBUsername_TextChanged(object sender, TextChangedEventArgs e)
@@ -48,17 +67,30 @@ namespace TriviaClient
 
         }
 
-        private void myButton_Click(object sender, RoutedEventArgs e)
+        private void Btn_BestScores_Click(object sender, RoutedEventArgs e)
         {
 
         }
 
         private void btn_signup_Click(object sender, RoutedEventArgs e)
         {
+            SignupWindow wind = new SignupWindow();
+            wind.Show();
+            this.Hide();
+            this.Close();
+
+            //NavigationService nav = NavigationService.GetNavigationService(this);
+            //Page1 p = new Page1();
+           // nav.Navigate(p);
 
         }
 
         private void TBPassword_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
         {
 
         }
---
+++ TriviaWPFClient/TriviaClient/MenuWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TriviaClient
+{
+    /// <summary>
+    /// Interaction logic for MenuWindow.xaml
+    /// </summary>
+    public partial class MenuWindow : Window
+    {
+        private string username;
+        public MenuWindow(string user)
+        {
+            username = user;
+            InitializeComponent();
+            TBHello.Text += username;
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TBHello_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Btn_CreateRoom_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Btn_joinRoom_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Btn_MyStatus_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Btn_Quit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void Btn_Logout_Click(object sender, RoutedEventArgs e)
+        {
+            TBHello.Text = username + " logged out!";
+        }
+    }
+}
