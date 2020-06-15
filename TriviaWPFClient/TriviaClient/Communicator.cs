77a1c12237514a2216ce6f93fb1b5772bd334973
Ofir Shapira <ofirs2003@gmail.com>
2020-06-15 17:11:01 +0300

    add connection to server

---
+++ TriviaWPFClient/TriviaClient/Communicator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace TriviaClient
+{
+    static class Communicator
+    {
+        public static Socket serverSocket;
+        public static bool Connect(string serverIp,int port)
+        {
+            Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPAddress ipaddress = System.Net.IPAddress.Parse(serverIp);
+            IPEndPoint ipEndPoint = new IPEndPoint(ipaddress, port);
+            try
+            {
+                sock.Connect(ipEndPoint);
+            }
+            catch(Exception e)
+            {
+                return false;
+            }
+            serverSocket = sock;
+            return true;
+        }
+
+    }
+}
