3bfbe0ce81e52949015c06bf373b8636b77edd10
Omri Zaiman <omri162004@gmail.com>
2020-06-18 12:06:42 +0300

    added CreateRoom request, respnse classes

---
+++ TriviaWPFClient/TriviaClient/CreateRoomRequest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriviaClient
+{
+    class CreateRoomRequest : Request
+    {
+        public const int MESSAGE_CODE = 40;
+        public string _roomName { get; set; }
+        public int _numOfPlayers { get; set; }
+        public int _numOfQuestions { get; set; }
+        public int _timePerQuestion { get; set; }
+        public CreateRoomRequest(string name, int players, int questions, int timeForQuestion) : base(MESSAGE_CODE) 
+        {
+            this._roomName = name;
+            this._numOfPlayers = players;
+            this._numOfQuestions = questions;
+            this._timePerQuestion = timeForQuestion;
+        }
+    }
+}
