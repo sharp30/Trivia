74bee6fbe25f44b677ef9fac1d95c871e8e08619
Omri Zaiman <omri162004@gmail.com>
2020-06-18 12:35:22 +0300

    added CreateRoomWindow class (gui and definitions)

--- TriviaWPFClient/TriviaClient/CreateRoomWindow.xaml.cs
+++ TriviaWPFClient/TriviaClient/CreateRoomWindow.xaml.cs
@@ -19,9 +19,48 @@ namespace TriviaClient
     /// </summary>
     public partial class CreateRoomWindow : Window
     {
-        public CreateRoomWindow()
+        public string username { set; get; }
+        public string roomName { set; get; }
+        public int numberOfPlayers { set; get; }
+        public int numberOfQuestions { set; get; }
+        public int TimeForQuestion { set; get; }
+        public CreateRoomWindow(string userName)
         {
+            this.username = userName;
             InitializeComponent();
+
+            usernameTB.Text += this.username;
+        }
+
+        private void BtnBackClick(object sender, RoutedEventArgs e)
+        {
+            MenuWindow wind = new MenuWindow(this.username);
+            wind.Show();
+            this.Hide();
+            this.Close();
+        }
+
+
+        private void BtnCreateClick(object sender, RoutedEventArgs e)
+        {
+            this.roomName = TBRoomName.Text;
+            this.numberOfPlayers = int.Parse(TBPlayers.Text);
+            this.numberOfQuestions = int.Parse(TBQuestions.Text);
+            this.TimeForQuestion = int.Parse(TBTimePerQuestion.Text);
+
+            CreateRoomResponse response = (CreateRoomResponse)Communicator.Communicate(
+                new CreateRoomRequest(this.roomName, this.numberOfPlayers, this.numberOfQuestions, this.TimeForQuestion));
+
+            if (response.status == 1)
+            {
+                //#TODO: move room creator to waiting room (to allow other players join)
+                /*
+                WaitingRoomWindow wind = new WaitingRoomWindow(params);
+                wind.Show();
+                this.Hide();
+                this.Close();
+                */
+            }
         }
     }
 }
