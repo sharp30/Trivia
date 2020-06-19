dd492bfbda9b9066ca4d8aa2519f6725fbe92892
Ofir Shapira <ofirs2003@gmail.com>
2020-06-19 14:53:00 +0300

    temp

---
+++ TriviaWPFClient/TriviaClient/BestScoresResponse.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriviaClient
+{
+    class BestScoresResponse : Response
+    {
+        public const int MESSAGE_CODE = 81;
+        public BestScoresResponse() : base(MESSAGE_CODE)
+        {
+            status = 1;
+        }
+        public string[] UserStatistics { get; set; }
+        public int status { get; set; }
+
+    }
+
+}
