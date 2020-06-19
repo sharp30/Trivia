c6ff89876f0b16458bb7fbb9e329b4fb98369525
Ofir Shapira <ofirs2003@gmail.com>
2020-06-19 15:14:08 +0300

    Fix Server!

--- TriviaServer/TriviaServer/Room.h
+++ TriviaServer/TriviaServer/Room.h
@@ -11,19 +11,21 @@ using std::string;
 class Room
 {
 public:
+	Room() = default;
 
-	Room(int roomId, LoggedUser creator);//for now
+	Room(int roomId, LoggedUser creator, string name, int maxPlayers, int questionTime, int questionAmount);
 
 	void addUser(LoggedUser userToAdd) throw();
 	void removeUser(LoggedUser userToRemove) throw();
 	bool isUserExist(LoggedUser userToSearch);
 	bool isActive();
-	void getAllUsers();//need to think on return type
+	vector<string> getAllUsers();
 	string getName() const;
 
 protected:
 	vector<LoggedUser> _users;
 	//room's data
+	int _questionAmount;
 	int _id;
 	string _name;
 	unsigned int _maxPlayers;
