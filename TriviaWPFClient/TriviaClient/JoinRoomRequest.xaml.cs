9f16caeb71fac49e2870f6c6f5764fe642cc575f
Omri Zaiman <omri162004@gmail.com>
2020-06-20 16:54:25 +0300

    updated JoinRoomWindow

--- TriviaWPFClient/TriviaClient/JoinRoomRequest.xaml.cs
+++ TriviaWPFClient/TriviaClient/JoinRoomRequest.xaml.cs
@@ -19,16 +19,40 @@ namespace TriviaClient
     /// </summary>
     public partial class JoinRoomRequest : Window
     {
+        public Room[] availiableRooms { set; get; }
+
         public JoinRoomRequest()
         {
             InitializeComponent();
 
-            //add textBoxes
+            //add buttons of rooms
             this.Hide();
-            Button btn = new Button { Content = "Room ", FontSize = 15, Name = "Room0" };
-            for (int i = 0; i < 5; i++)
+
+            UpdateRoomsListGrid();
+        }
+
+        /*
+        The function will rapidly ask the server to send the rooms list so it will be updated // Might be a thread
+        input: none
+        output: none
+        */
+        private void RefreshRoomsList()
+        {
+            //#TODO: get players list from server, if a change was detected, change the attribute array of players and call the UpdatePlayersListGrid function
+        }
+
+        /*
+        The function will add the new players that has joined to the grid
+        input: none
+        output: none
+        */
+        private void UpdateRoomsListGrid()
+        {
+            Button btn = null;
+
+            for (int i = 0; i < this.availiableRooms.Length; i++)
             {
-                btn = new Button { Content = "Room " + i.ToString() + "                    " + (i * 50).ToString(), FontSize = 40, Margin = new Thickness(90, 35 * i, 120, 35 * (i + 1)), Name = "room" + (i+1).ToString() };
+                btn = new Button { Content = this.availiableRooms[i].roomName, FontSize = 25, Margin = new Thickness(90, 35 * i, 120, 35 * (i + 1)) };
                 Grid.SetRow(btn, i);
                 mainPart.Children.Add(btn);
             }
