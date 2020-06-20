1e5742d1483e0bc681ac91f995aa4f62e220d17b
Omri Zaiman <omri162004@gmail.com>
2020-06-20 15:19:25 +0300

    fixed bug in menurequesthandler in createRoom func

--- TriviaServer/TriviaServer/MenuRequestHandler.cpp
+++ TriviaServer/TriviaServer/MenuRequestHandler.cpp
@@ -6,9 +6,6 @@
 #include "LogoutResponse.h"
 #include "CreateRoomResponse.h"
 #include "GetRoomsResponse.h"
-
-//initialize messages codes
-map<string, int> MenuRequestHandler::MSG_CODES = MenuRequestHandler::initMsgCodes();
  
 //-----------------constructor--------------------
 MenuRequestHandler::MenuRequestHandler(RequestHandlerFactory* factory ,LoggedUser user) :IRequestHandler(factory), m_user(user)
@@ -33,7 +30,8 @@ RequestResult MenuRequestHandler::createRoom(RequestInfo info)
 	RequestResult res;
 	try
 	{
-		(this->m_handlerFactory->getRoomManager()).createRoom(m_user.getUsername());
+		(this->m_handlerFactory->getRoomManager()).createRoom(m_user.getUsername(), request.getRoomName(),
+			request.getMaxUsers(), request.getAnswerTimeOut(), request.getQuestionCount());
 	}
 	catch(std::exception e)
 	{
--- TriviaServer/TriviaServer/MenuRequestHandler.h
+++ TriviaServer/TriviaServer/MenuRequestHandler.h
@@ -26,16 +26,7 @@ protected:
 
 
 	static map<string, int> MSG_CODES;
-	static map<string, int> initMsgCodes()
-	{
-		map<string, int> m;
-		m["CREATE_ROOM"] = 40;
-		m["JOIN_ROOM"] = 44;
-		m["GET_STATS"] = 70;
-		m["LOGOUT"] = 100;
-
-		return m;
-	};
+	
 
 	LoggedUser m_user;
 	static const std::map<int, handler_func> m_functions;
