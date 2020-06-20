98086ea8dd5fe208bc19b31c32fdbd76bf132567
Omri Zaiman <omri162004@gmail.com>
2020-06-20 16:45:30 +0300

    added dynamic text blocks to The waiting room

--- TriviaWPFClient/TriviaClient/WaitingRoomWindow.xaml.cs
+++ TriviaWPFClient/TriviaClient/WaitingRoomWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -47,6 +48,35 @@ namespace TriviaClient
             maxplayersTB.Text += this.room.numberOfPlayers;
             questAmountTB.Text += this.room.numberOfQuestions;
             questTimeTB.Text += this.room.TimeForQuestion;
+
+            UpdatePlayersListGrid();
+        }
+
+        /*
+        The function will rapidly ask the server to send the players list so it will be updated // Might be a thread
+        input: none
+        output: none
+        */
+        private void RefreshPlayersList()
+        {
+            //#TODO: get players list from server, if a change was detected, change the attribute array of players and call the UpdatePlayersListGrid function
+        }
+
+        /*
+        The function will add the new players that has joined to the grid
+        input: none
+        output: none
+        */
+        private void UpdatePlayersListGrid()
+        {
+            TextBlock txt = null;
+
+            for (int i = 0; i < this.players.Length ; i++)
+            {
+                txt = new TextBlock { Text = this.players[i], FontSize = 30, Margin = new Thickness(90, 35 * i, 120, 35 * (i + 1)) };
+                Grid.SetRow(txt, i);
+                mainPart.Children.Add(txt);
+            }
         }
     }
 }
