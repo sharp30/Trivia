3a0e967aafaffe25f094173c0854231c22833c24
Ofir Shapira <ofirs2003@gmail.com>
2020-07-03 17:41:03 +0300

    finish GameData. updated Game.h

--- TriviaServer/TriviaServer/Game.h
+++ TriviaServer/TriviaServer/Game.h
@@ -1,14 +1,26 @@
 #pragma once
 #include <vector>
 #include <map>
+#include "LoggedUser.h"
+#include "Question.h"
 
 using std::vector;
 using std::map;
 
+
+
+typedef struct GameData 
+{
+	int currentQuestion;
+	unsigned int correctAnswerCount;
+	unsigned int incorrectAnswerCount;
+	unsigned int averangeAnswerTime;
+} GameData;
+
 class Game
 {
 public:
-	Game();
+	Game(vector<Question> questions,map<LoggedUser> players);
 	Question getQuestionForUser(LoggedUser user);
 	void submitAnswer();
 	void removePlayer();
@@ -16,3 +28,4 @@ protected:
 	vector<Question> m_questions;
 	map<LoggedUser, GameData> m_players;
 };
+
