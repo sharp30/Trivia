fbd3fc07950da480ee2cf9cfcc3a2169c0e6c0d6
Ofir Shapira <ofirs2003@gmail.com>
2020-07-05 14:22:34 +0300

    the background is added

--- TriviaWPFClient/TriviaClient/EndGameWindow.xaml.cs
+++ TriviaWPFClient/TriviaClient/EndGameWindow.xaml.cs
@@ -42,7 +42,7 @@ namespace TriviaClient
             int count = 1;
             
             //-----print winner------
-            txt = new TextBlock { Text = "The winner is " + FindWinner() + "!", FontSize = 40, Margin = new Thickness(90, 35 * count, 120, 35 * (count + 1)) };
+            txt = new TextBlock { Text = "The winner is " + FindWinner() + "!", FontSize = 40, Margin = new Thickness(90, 35 * count, 120, 35 * (count + 1)), Foreground = Brushes.White };
 
             Grid.SetRow(txt, count);
             mainPart.Children.Add(txt);
@@ -51,7 +51,7 @@ namespace TriviaClient
 
             foreach (PlayerResults res in this.results)
             {
-                txt = new TextBlock { Text = res.ToString(), FontSize = 40, Margin = new Thickness(90, 35 * count, 120, 35 * (count + 1)) };
+                txt = new TextBlock { Text = res.ToString(), FontSize = 40, Margin = new Thickness(90, 35 * count, 120, 35 * (count + 1)), Foreground = Brushes.White };
 
                 Grid.SetRow(txt, count);
                 mainPart.Children.Add(txt);
